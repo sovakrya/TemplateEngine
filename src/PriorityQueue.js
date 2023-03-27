d65ebfc2ba0164ea192ab7f4ae8934116f1a9688
Yana <tyan_34ee@mail.ru>
2023-03-27 15:17:19 +0300

    add PriorityQueue and use it

---
+++ src/PriorityQueue.js
@@ -0,0 +1,90 @@
+export class PriorityQueue {
+  #queue;
+  /**
+   * Функция которая проверяет, что a имеет больший приоритет чем b
+   * @type { (a, b) => boolean }
+   */
+  #comparator;
+
+  constructor(comparator) {
+    this.#queue = [];
+    this.#comparator = comparator;
+  }
+
+  getElement() {
+    this.#swap(0, this.#queue.length - 1);
+
+    const element = this.#queue.pop();
+
+    this.#heapifyDown(0);
+
+    return element;
+  }
+
+  addElement(element) {
+    this.#queue.push(element);
+
+    this.#heapifyUp(this.#queue.length - 1);
+  }
+
+  get length() {
+    return this.#queue.length;
+  }
+
+  #heapifyUp(idx) {
+    if (idx === 0) return;
+
+    const item = this.#queue.at(idx);
+    const parentIdx = this.#getParentIdx(idx);
+    const parent = this.#queue.at(parentIdx);
+
+    if (this.#comparator(item, parent)) {
+      this.#swap(idx, parentIdx);
+      this.#heapifyUp(parentIdx);
+      return;
+    }
+  }
+
+  #heapifyDown(idx) {
+    if (idx === this.#queue.length - 1) return;
+
+    const parent = this.#queue.at(idx);
+    let childIdx;
+
+    const leftChildIdx = this.#getLeftChildIdx(idx);
+    const rightChildIdx = this.#getRightChildIdx(idx);
+    const leftChild = this.#queue.at(leftChildIdx);
+    const rightChild = this.#queue.at(rightChildIdx);
+
+    if (!rightChild) {
+      childIdx = leftChildIdx;
+    } else if (this.#comparator(leftChild, rightChild)) {
+      childIdx = leftChildIdx;
+    } else {
+      childIdx = rightChildIdx;
+    }
+
+    if (this.#comparator(this.#queue.at(childIdx), parent)) {
+      this.#swap(childIdx, idx);
+      this.#heapifyDown(childIdx);
+    }
+  }
+
+  #getLeftChildIdx(idx) {
+    return idx * 2 + 1;
+  }
+
+  #getRightChildIdx(idx) {
+    return idx * 2 + 2;
+  }
+
+  #getParentIdx(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
+
+  #swap(a, b) {
+    const tmp = this.#queue.at(b);
+    this.#queue[b] = this.#queue[a];
+    this.#queue[a] = tmp;
+  }
+}
--- src/TemplateEngine.js
+++ src/TemplateEngine.js
@@ -1,99 +1,88 @@
-function copy(el, n) {
-  const elements = Array.from({ length: n }, () => el.cloneNode(true));
-  el.after(...elements);
-}
+import { PriorityQueue } from "./PriorityQueue";
 
-function remove(el, n) {
-  for (n; n > 0; n--) {
-    if (!el.nextElementSibling) return;
+const engine = {
+  copy(el, n) {
+    const elements = Array.from({ length: n }, () => el.cloneNode(true));
+    el.after(...elements);
+  },
 
-    el.nextElementSibling.remove();
-  }
-}
+  remove(el, n) {
+    for (n; n > 0; n--) {
+      if (!el.nextElementSibling) return;
 
-function removeChildren(el, n) {
-  if (!el.parentNode) return;
+      el.nextElementSibling.remove();
+    }
+  },
 
-  for (n; n > 0; n--) {
-    if (!el.firstElementChild) return;
+  removeChildren(el, n) {
+    if (!el.parentNode) return;
 
-    el.firstElementChild.remove();
-  }
-}
+    for (n; n > 0; n--) {
+      if (!el.firstElementChild) return;
 
-function switchEl(el, n) {
-  const parent = el.parentNode;
+      el.firstElementChild.remove();
+    }
+  },
 
-  if (!parent) return;
+  switch(el, n) {
+    const parent = el.parentNode;
 
-  if (parent.childElementCount === 1) return;
+    if (!parent) return;
 
-  let newEl = el;
+    if (parent.childElementCount === 1) return;
 
-  for (n; n > 0; n--) {
-    newEl = newEl.nextElementSibling;
+    let newEl = el;
+
+    for (n; n > 0; n--) {
+      newEl = newEl.nextElementSibling;
+
+      if (!newEl) {
+        newEl = parent.firstElementChild;
+      }
+    }
 
     if (!newEl) {
       newEl = parent.firstElementChild;
     }
-  }
 
-  if (!newEl) {
-    newEl = parent.firstElementChild;
-  }
+    let tmp = document.createElement("a");
+    let tmp1 = document.createElement("a");
+    newEl.before(tmp);
+    el.before(tmp1);
+    tmp.replaceWith(el);
+    tmp1.replaceWith(newEl);
+  },
+};
 
-  let tmp = document.createElement("a");
-  let tmp1 = document.createElement("a");
-  newEl.before(tmp);
-  el.before(tmp1);
-  tmp.replaceWith(el);
-  tmp1.replaceWith(newEl);
-}
+const prioritys = {
+  copy: 1,
+  remove: 2,
+  removeChildren: 3,
+  switch: 4,
+};
 
 function solution(entryPoint) {
-  const copyQueue = [];
-  const removeQueue = [];
-  const removeChildrenQueue = [];
-  const swithQueue = [];
-
-  const queues = {
-    copy: copyQueue,
-    remove: removeQueue,
-    removeChildren: removeChildrenQueue,
-    switch: swithQueue,
-  };
+  // { priority: number, name: string, count: number, el }
+  const queue = new PriorityQueue((a, b) => {
+    return a.priority < b.priority;
+  });
 
   for (let child of entryPoint.children) {
-    const atrr = child.getAttribute("x-make");
+    const attr = child.getAttribute("x-make");
 
-    if (!atrr) continue;
+    if (!attr) continue;
 
-    const [name, n] = atrr.split(":");
+    const [name, n] = attr.split(":");
 
-    queues[name].push([child, n]);
+    queue.addElement({ priority: prioritys[name], name, count: n, el: child });
 
     child.removeAttribute("x-make");
   }
 
-  while (copyQueue.length) {
-    const arg = copyQueue.shift();
+  while (queue.length) {
+    const { count, el, name } = queue.getElement();
 
-    copy(...arg);
-  }
-  while (removeQueue.length) {
-    const arg = removeQueue.shift();
-
-    remove(...arg);
-  }
-  while (removeChildrenQueue.length) {
-    const arg = removeChildrenQueue.shift();
-
-    removeChildren(...arg);
-  }
-  while (swithQueue.length) {
-    const arg = swithQueue.shift();
-
-    switchEl(...arg);
+    engine[name](el, count);
   }
 
   for (let child of entryPoint.children) {
